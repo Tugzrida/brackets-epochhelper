5a748f327d4e57cb4d332cf5c42bf89ef5e40a0a
Tugzrida <Tugzrida@users.noreply.github.com>
2018-07-15 22:13:23 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# brackets-epochhelper
+
+Shows the value of any selected UNIX epoch timestamp (in micro, milli or whole seconds, hex or decimal) as an ISO datetime in the status bar.
+
+Mad props to the awesome website [epochconverter.com](https://www.epochconverter.com/), where a decent chunk of the code in this extension is from.
+
+![epochhelper in action](https://i.imgur.com/IEmPjna.gif "epochhelper in action")
---
+++ main.js
@@ -0,0 +1,64 @@
+/* global define, brackets, $ */
+define(function () {
+    "use strict";
+
+    var EditorManager = brackets.getModule('editor/EditorManager'),
+        $stampOut = $("<span class='epochhelper-label' />");
+
+    $stampOut.appendTo("#status-info");
+
+    function isHex(h) {
+        var a = parseInt(h, 16);
+        return (a.toString(16) === h.toLowerCase());
+    }
+
+    function showTimestamp(event, editor) {
+        editor = editor || EditorManager.getActiveEditor();
+        var inputtext = editor.getSelectedText(),
+            assumption = "";
+
+        if (inputtext.length === 0) {
+            $stampOut.text("");
+            return;
+        }
+
+        inputtext = inputtext.replace(/[`'"\s]+/g, '');
+        if (inputtext.charAt(inputtext.length - 1) === "L") {
+            inputtext = inputtext.slice(0, -1);
+        }
+
+        if (isNaN(inputtext)) {
+            if (isHex(inputtext)) {
+                inputtext = '0x' + inputtext;
+            } else {
+                $stampOut.text("");
+                return;
+            }
+        }
+
+        inputtext = inputtext * 1;
+        if ((inputtext >= 100000000000000) || (inputtext <= -100000000000000)) {
+            assumption = " (assuming microseconds)";
+            inputtext = Math.round(inputtext / 1000);
+        } else if ((inputtext >= 100000000000) || (inputtext <= -100000000000)) {
+            assumption = " (assuming milliseconds)";
+        } else {
+            assumption = " (assuming seconds)";
+            inputtext = (inputtext * 1000);
+        }
+
+        if (inputtext < -6857222400000) {
+            assumption += " (pre-Gregorian!)";
+        }
+
+        $stampOut.text(new Date(inputtext).toISOString() + assumption);
+    }
+
+    function onEditorChange(event, current, previous) {
+        previous.on("cursorActivity.statusbar", showTimestamp);
+        current.on("cursorActivity.statusbar", showTimestamp);
+        showTimestamp(null, current);
+    }
+
+    EditorManager.on("activeEditorChange", onEditorChange);
+});
